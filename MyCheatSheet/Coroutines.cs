6204fe25179784afe34edde9f0d29dd80cd89136
Bekhruz Gab <brozermanik@gmail.com>
2021-07-04 01:31:19 +0300

    added Enums, Coroutines

---
+++ MyCheatSheet/Coroutines.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyCheatSheet
+{
+    public class Coroutines
+    {
+        public GameObject obj;
+
+        void Start()
+        {
+            Invoke("Inst, 2f");
+        }
+        
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                StartCoroutine(instObj());
+            }
+        }
+
+        void Inst()
+        {
+            Instantiate(obj, new Vector3 (Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f)), Quaternion.identity);
+        }
+        IEnumerator instObj()
+        {
+            while (true)
+            {
+                Instantiate(obj, new Vector3 (Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f)), Quaternion.identity);
+                yield return new WaitForSeconds(1.5f);
+            }
+        }
+    }
+}
---
+++ MyCheatSheet/IEnumsomething.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+
+namespace MyCheatSheet
+{
+    /// <summary>
+    /// Простой варивант
+    /// </summary>
+    /*
+    public class IEnumsomething
+    {
+        class Week : IEnumerable
+        {
+            string[] days =
+            {
+                "Monday", "Tuesday", "Wednesday", "Thursday",
+                "Friday", "Saturday", "Sunday"
+            };
+
+            public IEnumerator GetEnumerator()
+            {
+                return days.GetEnumerator();
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Week week = new Week();
+                foreach (var day in week)
+                {
+                    Console.WriteLine(day);
+                }
+
+                Console.Read();
+            }
+        }
+    }
+    */
+    {
+    class WeekEnumerator : IEnumerator
+    {
+        string[] days;
+        int position = -1;
+        public WeekEnumerator(string[] days)
+        {
+            this.days = days;
+        }
+        public object Current
+        {
+            get
+            {
+                if (position == -1 || position >= days.Length)
+                    throw new InvalidOperationException();
+                return days[position];
+            }
+        }
+ 
+        public bool MoveNext()
+        {
+            if(position < days.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+                return false;
+        }
+ 
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+    class Week
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday", "Saturday", "Sunday" };
+ 
+        public IEnumerator GetEnumerator()
+        {
+            return new WeekEnumerator(days);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Week week = new Week();
+            foreach(var day in week)
+            {
+                Console.WriteLine(day);
+            }
+            Console.Read();
+        }
+    }
+    }
+}
