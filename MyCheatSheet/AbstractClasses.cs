2baf2da02e891757d7db00fac702c65e4dec5ab9
Bekhruz Gab <brozermanik@gmail.com>
2021-07-03 20:11:47 +0300

    added abstract class

---
+++ MyCheatSheet/AbstractClasses.cs
@@ -0,0 +1,36 @@
+﻿namespace MyCheatSheet
+{
+    public class AbstractClasses
+    {
+        // абстрактный класс фигуры
+        abstract class Figure
+        {
+            // абстрактный метод для получения периметра
+            public abstract float Perimeter();
+            // абстрактный метод для получения площади
+            public abstract float Area();
+        }
+// производный класс прямоугольника
+        class Rectangle : Figure
+        {
+            public float Width { get; set; }
+            public float Height { get; set; }
+ 
+            public Rectangle(float width, float height)
+            {
+                this.Width = width;
+                this.Height = height;
+            }
+            // переопределение получения периметра
+            public override float Perimeter()
+            {
+                return Width * 2 + Height * 2;
+            }
+            // переопрелеление получения площади
+            public override float Area()
+            {
+                return Width * Height;
+            }
+        }
+    }
+}
